31024a4da8dd0cebdb5ed7e901fcec7313101ab0
Dan Hodge <danhodge@gmail.com>
2022-07-22 21:50:32 -0400

    update quotes to return the closest adj closing price

--- src/harvest/quotes.py
+++ src/harvest/quotes.py
@@ -1,28 +1,62 @@
 import csv
+from dataclasses import dataclass
 from datetime import date, timedelta
 from decimal import Decimal
-from typing import Sequence
+from typing import Callable, Dict, List, Sequence
 import time
 import requests
 
 
-def lookup_prices(symbols: Sequence[str], date: date):
-    start_time = int(time.mktime((date - timedelta(days=7)).timetuple()))
-    end_time = int(time.mktime(date.timetuple()))
+@dataclass(frozen=True)
+class Quote:
+    date: date
+    price: Decimal
 
+
+def yahoo_finance_fetcher(symbol: str, date: date, lookback_days=7) -> str | None:
+    start_time = int(time.mktime((date - timedelta(days=lookback_days)).timetuple()))
+    end_time = int(time.mktime(date.timetuple()))
+    url = f"https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1={start_time}&period2={end_time}&interval=1d&events=history&includeAdjustedClose=true"
+
+    resp = requests.get(url, headers={"user-agent": "curl/7.79.1"})
+    if resp.status_code == 200:
+        return resp.text
+    else:
+        return None
+
+
+def lookup_prices(symbols: Sequence[str], date: date):
     results = {}
     for symbol in symbols:
-        url = f"https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1={start_time}&period2={end_time}&interval=1d&events=history&includeAdjustedClose=true"
-        resp = requests.get(url, headers={"user-agent": "curl/7.79.1"})
-        if resp.status_code == 200:
-            rows = list(csv.reader(resp.text.splitlines()))
-            header = rows[0]
-            results[symbol] = list(map(lambda row: transform(header, row), rows[1:]))
+        if quote := fetch_quote(symbol=symbol, date=date):
+            results[symbol] = quote
 
     return results
 
 
-def transform(header, row):
+def fetch_quote(
+    symbol: str,
+    date: date,
+    fetcher: Callable[[str, date], str | None] = yahoo_finance_fetcher,
+) -> Quote | None:
+    result = fetcher(symbol, date)
+    quotes = to_quotes(result) if result else []
+
+    for quote in sorted(quotes, key=lambda quote: quote.date, reverse=True):
+        if quote.date <= date:
+            return quote
+
+
+def to_quotes(price_data: str) -> List[Quote]:
+    rows = list(csv.reader(price_data.splitlines()))
+    header = rows[0]
+    return map(
+        lambda rec: Quote(date=rec["Date"], price=rec["Adj Close"]),
+        map(lambda row: transform(header, row), rows[1:]),
+    )
+
+
+def transform(header: Sequence, row: Sequence) -> Dict:
     def transformer(t):
         if t[0] == "Date":
             return t[0], date.fromisoformat(t[1])
